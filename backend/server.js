80257276b4e12d4821ed790127eaad48b54c1721
Bjorn van der Lugt <bjorn.vd.lugt@gmail.com>
2025-03-22 07:01:57 +0100

    Added new feature to portfolio

--- backend/server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app'); // Ensure this matches the location of your app.js
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
